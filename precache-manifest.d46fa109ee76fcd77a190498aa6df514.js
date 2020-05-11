a59d1d925b11f839d3ca372ea5ea67e1a3d4bc52
Senay <mola.maru77@gmail.com>
2020-05-11 18:07:41 +0200

    Updates

---
+++ precache-manifest.d46fa109ee76fcd77a190498aa6df514.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2993f792feb397b5e22177c2b1934106",
+    "url": "/tv_guide/index.html"
+  },
+  {
+    "revision": "a73b15809817a8d6b0f0",
+    "url": "/tv_guide/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "1ae7f7784d1f6029256b",
+    "url": "/tv_guide/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a73b15809817a8d6b0f0",
+    "url": "/tv_guide/static/js/2.1519b665.chunk.js"
+  },
+  {
+    "revision": "b651d5533fc80509587379dd07cc06bd",
+    "url": "/tv_guide/static/js/2.1519b665.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1ae7f7784d1f6029256b",
+    "url": "/tv_guide/static/js/main.d6a860b6.chunk.js"
+  },
+  {
+    "revision": "415773fd84f8cf05b805",
+    "url": "/tv_guide/static/js/runtime-main.d19faecb.js"
+  },
+  {
+    "revision": "4105d0f74f51b7a41160cef70094fef8",
+    "url": "/tv_guide/static/media/eservices.4105d0f7.jpg"
+  }
+]);
